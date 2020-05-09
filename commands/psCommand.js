9e713792793996b5bfce8bdd014f0e24cc6832a5
John A Stevens <john@otherskylines.com>
2020-05-09 15:41:39 -0600

    Add psCommand

---
+++ commands/psCommand.js
@@ -0,0 +1,23 @@
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
+class PsCommand {
+    constructor(prefix, users) {
+        this.prefix = prefix
+        this.command = prefix + "ps"
+        this.helpText = `\`${this.command}\` - Show info on hosted processes`
+        this.users = users
+    }
+
+    canHandle(command) {
+        return command === this.command;
+    }
+
+    async handle(args) {
+        const shellCommand = `ps -U ${this.users.join(',')} -o pid,pcpu,pmem,args`
+        const psOutput = await exec(shellCommand)
+        return ['`!ps` - host process info:', `\`>${shellCommand}\``, ...psOutput.stdout.split('\n').filter(line => line.trim().length > 0).map(line => `\`${line}\``)]
+    }
+}
+
+module.exports = PsCommand;
