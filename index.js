9ebad8852244d60f1a0423592b404845aa5c21d3
John A Stevens <john@otherskylines.com>
2020-05-10 10:03:01 -0600

    Add pino logging

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const Discord = require('discord.js');
+const pino = require('pino')
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const PingCommand = require('./commands/pingCommand')
 const ListCommand = require('./commands/listCommand')
 const UnknownCommand = require('./commands/unknownCommand')
 const PsCommand = require('./commands/psCommand')
+
+const logger = pino()
 
 const commands = [
     new PingCommand('!'),
@@ -20,17 +23,28 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+const sendToChannel = function(channel,message) {
+    logger.info(`(Send) To: ${channel.guild}#${channel.name} Content: ${message}`)
+    channel.send(message)
+}
+
+const logReceive = function(message) {
+    logger.info(`(Receive) From: ${message.author.username} <@${message.author.id}> On: ${message.channel.guild}#${message.channel.name} (${message.channel.id}) Content: ${message.content}`)
+}
+
 // Create an event listener for messages
 client.on('message', async (message) => {
   if(message.content.startsWith('!')) {
+      logReceive(message)
       const tokens = message.content.split(/(\s+)/).filter(e => e.trim().length > 0);
 
       for (let command of commands) {
           if(command.canHandle(tokens[0])) {
-              message.channel.send(`<@${message.author.id}> - \`${tokens[0]}\``)
+              sendToChannel(message.channel, `<@${message.author.id}> - \`${tokens[0]}\``)
+              
               const responseLines = await command.handle(tokens.slice(1));
               for (const line of responseLines) {
-                message.channel.send(line);
+                sendToChannel(message.channel, line);
               }
               break;
           }
